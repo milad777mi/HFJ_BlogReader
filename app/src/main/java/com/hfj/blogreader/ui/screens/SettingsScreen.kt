7ad98306740bd89a14cb3f21ffba279b7c7f3b5d
mj9246N <mjfull603@gmail.com>
2026-09-04 21:36:42 +0330

    Update SettingsScreen.kt

--- app/src/main/java/com/hfj/blogreader/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/hfj/blogreader/ui/screens/SettingsScreen.kt
@@ -9,13 +9,13 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.collectAsState   // ✅ اصلاح شده
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hfj.blogreader.ui.theme.LocalFontScale
 import com.hfj.blogreader.viewmodel.MainViewModel
-import kotlinx.coroutines.flow.collectAsState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
