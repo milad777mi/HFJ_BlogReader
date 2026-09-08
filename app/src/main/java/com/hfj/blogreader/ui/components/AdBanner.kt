906dd9a3b12bc4b16879f3ee46123ce2693e6b4b
milad777mi <mhmdkwarkw@gmail.com>
2026-09-08 14:54:52 +0330

    Update AdBanner.kt
    تبلیغات جدید

--- app/src/main/java/com/hfj/blogreader/ui/components/AdBanner.kt
+++ app/src/main/java/com/hfj/blogreader/ui/components/AdBanner.kt
@@ -3,6 +3,7 @@ package com.hfj.blogreader.ui.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
