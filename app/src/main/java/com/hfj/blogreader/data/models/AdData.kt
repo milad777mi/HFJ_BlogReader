93d1ae21096e1830312b932c3215e361e7edcd3d
milad777mi <mhmdkwarkw@gmail.com>
2026-09-19 17:24:09 +0330

    Update AdData.kt

--- app/src/main/java/com/hfj/blogreader/data/models/AdData.kt
+++ app/src/main/java/com/hfj/blogreader/data/models/AdData.kt
@@ -1,9 +1,1 @@
-package com.hfj.blogreader.data.models
-
-data class AdData(
-    val exists: Boolean = false,
-    val imageUrl: String? = null,
-    val link: String? = null,
-    val title: String? = null,
-    val updatedAt: Long = 0L
-)
+//k errorrr
